f5578f1874d9891f93734ef442d148071c248d34
Navya Mandru <47963922+NavyaMandru@users.noreply.github.com>
2024-04-18 20:58:36 -0400

    Create BestTimeToBuySellStock.py

---
+++ BestTimeToBuySellStock.py
@@ -0,0 +1,22 @@
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+# Time complexity - O(n)
+# Space complexity - O(1)
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        buy = prices[0]
+        for sell in prices:
+            if sell > buy:
+                profit = max(profit, sell-buy)
+            else:
+                buy = sell
+        return profit
+        
