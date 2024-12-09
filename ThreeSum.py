ff8005d5632baafebdfe0ddf5e614db639e7dd8f
NavyaMandru <navyamandru084@gmail.com>
2024-12-09 13:56:43 -0500

    fix

--- ThreeSum.py
+++ ThreeSum.py
@@ -1,4 +1,32 @@
-# TODO
-# Will do it later
+class Solution:
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
 
-# test
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            j = i + 1
+            k = len(nums) - 1
+
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j += 1
+                else:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+
+        return res
+
+
+nums = [1, -1, 0, 2, -2, 3]
+obj = Solution()
+res = obj.threeSum(nums)
+print(res)
