beedcad76d43877cc865f0fec96203a6e9e56aa4
NavyaMandru <navyamandru084@gmail.com>
2024-12-12 14:19:06 -0500

    deployee flask app

--- test_BinarySearch.py
+++ test_BinarySearch.py
@@ -1,6 +1,6 @@
 import pytest
 from BinarySearch import binary_search
 
-def test_binary_search():
-  assert binary_search([1,2,3,4], 4) == 3
 
+def test_binary_search():
+    assert binary_search([1, 2, 3, 4], 4) == 3
